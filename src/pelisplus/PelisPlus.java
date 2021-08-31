40c0d5be46cde1f4842b5583fc18bae58f91dfcc
Antonio Barrera <jbarreram9@miumg.edu.gt>
2021-08-30 20:53:32 -0600

    Probando cambio de rama

--- src/pelisplus/PelisPlus.java
+++ src/pelisplus/PelisPlus.java
@@ -40,6 +40,7 @@ public class PelisPlus {
                         //1. Creamos el objeto que administra el catalogo de personas
                         //La creacion del archivo es opcional, de todas maneras se creara 
                         //al escribir por primera vez en el archivo
+                        //Probando cambio de rama
                         catalogoPeli.iniciarArchivo(nombreArchivo);
                         break;
                     case 2:
